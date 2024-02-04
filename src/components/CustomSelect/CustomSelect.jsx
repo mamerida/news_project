ee07d6642642ccea811038988cc5b95f16365ca6
mamerida <mamerida2018@gmail.com>
2024-02-04 16:10:52 -0300

    [PATCH] Add select component

---
+++ src/components/CustomSelect/CustomSelect.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import "./CustomSelect.css"
+
+const defaultEmptyOption = [{value:"", label:""}];
+
+
+/**
+ * Select to the platform.
+ * @param {Object[]} options - The options to select.
+ * @param {string} options.value - Value of the select, value to use when the option are seleted
+ * @param {string} options.label - String to show in select.
+ * @param {string} name - name to form manager.
+ * @param {string} value - value to form manager.
+ * @callback onChange - action to execute when a option is selected.
+ * @param {boolean=true} emptyOption - that prop add a empty option in the select .
+ */
+export const CustomSelect = ({options = [], name, value, onChange, emptyOption=true}) => {
+    const [optionsSelect, setOptionsSelect] = useState(options)
+
+    useEffect(()=>{
+        if(emptyOption) setOptionsSelect([...defaultEmptyOption,...options])
+    },[options,emptyOption])
+
+    return (
+    <>
+        <select name={name} onChange={onChange} value={value} className={"custom_select"}>
+            {optionsSelect.map((opt)=>{
+                return <option key={opt.value} value={opt.value}>{opt.label}</option>
+            })}
+        </select>
+    </>
+    )
+}
