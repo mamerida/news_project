5e972e08778ae36a77c29c1d4ed34be33fa13be5
mamerida <mamerida2018@gmail.com>
2024-02-06 19:19:26 -0300

    [PATCH] Add card list

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Card.css';
+import { Button } from '../Button/Button';
+
+const AUTHOR_NOT_AVAILABLE = "Author not available"
+const IMAGE_BACKUP_URL = "https://placehold.co/330x220"
+
+export const Card = ({title, author, urlImage, description}) => {
+  return (
+    <section className='card_container'>
+      <div className='card'>
+        <header className='card_header'>
+          <img
+            src={urlImage || IMAGE_BACKUP_URL}
+            alt='card_cover'
+          />
+        </header>
+        <section className='card_content'>
+          <div className='title_container'>
+            <span className='card_title'>{title}</span>
+            <span className='card_author'> Author : {author || AUTHOR_NOT_AVAILABLE}</span>
+          </div>
+          <div className='paragraph_container'> 
+            <p>
+              {description}
+            </p>
+          </div>
+        </section>
+      </div>
+    </section>
+  )
+}
